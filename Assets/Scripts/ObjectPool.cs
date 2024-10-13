60c3c748bce3bb2038667aca5e42811ef8d51bad
Dany Carrillo <danycarrito27@outlook.com>
2024-10-13 01:15:23 -0600

    Ruleta

---
+++ Assets/Scripts/ObjectPool.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectPool : MonoBehaviour
+{
+    public GameObject prefab; 
+    public int poolSize = 10; 
+
+    private List<GameObject> pool; 
+
+    void Start()
+    {
+        pool = new List<GameObject>();
+
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject obj = Instantiate(prefab);
+            obj.SetActive(false); 
+            pool.Add(obj); 
+        }
+    }
+
+    public GameObject GetPooledObject()
+    {
+        foreach (GameObject obj in pool)
+        {
+            if (!obj.activeInHierarchy)
+            {
+                return obj;
+            }
+        }
+
+        GameObject newObj = Instantiate(prefab);
+        newObj.SetActive(false);
+        pool.Add(newObj);
+        return newObj;
+    }
+
+    public void ReturnObjectToPool(GameObject obj)
+    {
+        obj.SetActive(false);
+    }
+}

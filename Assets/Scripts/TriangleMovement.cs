a1231b60bf4ca5b61ada403b4253e73811d990f5
Dany Carrillo <danycarrito27@outlook.com>
2024-10-20 22:34:36 -0600

    lol

--- Assets/Scripts/TriangleMovement.cs
+++ Assets/Scripts/TriangleMovement.cs
@@ -1,24 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class TriangleMovement : MonoBehaviour
 {
     public float speed;
     public float force = 10f;
+    public float timeToRespawn;
+    public Vector2 spawnPosition;
 
-    private float timerRespawn;
+    private float timer;
     private Rigidbody2D rb;
+    public ObjectPool objectPool;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
     }
 
     private void Update()
     {
-        timerRespawn += Time.deltaTime;
+        timer += Time.deltaTime;
     }
 
     void FixedUpdate()
@@ -26,7 +30,18 @@ public class TriangleMovement : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             rb.AddForce(Vector3.up * force, ForceMode2D.Impulse);
-            //if(timerRespawn <)
+
+            if (timer < timeToRespawn)
+            {
+                timer = 0;
+                GameObject pooledObject = objectPool.GetPooledObject();
+
+                if (pooledObject != null)
+                {
+                    pooledObject.transform.position = spawnPosition;
+                    pooledObject.SetActive(true); 
+                }
+            }
         }
-    } 
+    }
 }

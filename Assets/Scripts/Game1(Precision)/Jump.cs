904ae25219b128d6731baece94eb4897177992eb
Dany Carrillo <danycarrito27@outlook.com>
2024-11-01 11:43:22 -0600

    animaciones

--- Assets/Scripts/Game1(Precision)/Jump.cs
+++ Assets/Scripts/Game1(Precision)/Jump.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using DG.Tweening;
 
 public class Jump : MonoBehaviour
 {
     public float jumpForce; 
     public bool isGrounded = false; 
     private Rigidbody2D rb;
+
+
 
     void Start()
     {
@@ -26,12 +29,17 @@ public class Jump : MonoBehaviour
     {
         rb.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
         isGrounded = false;
+
+        Sequence s = DOTween.Sequence();
+        s.Append(transform.DOScale(2, 1)).Append(transform.DOScale(1, 1));
+        //s.Append(transform.DORotate(Vector3.right*100,200).SetLoops(2));
     }
 
     void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
+
             isGrounded = true;
         }
     }

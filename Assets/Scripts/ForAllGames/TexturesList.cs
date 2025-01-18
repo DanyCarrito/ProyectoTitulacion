c36c73bc270e38aef00403fb2aa32fd6bb9e9219
Dany Carrillo <danycarrito27@outlook.com>
2025-01-18 00:03:56 -0600

    texturas en todos loa mundo s

--- Assets/Scripts/ForAllGames/TexturesList.cs
+++ Assets/Scripts/ForAllGames/TexturesList.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class TexturesList : MonoBehaviour
 {
     public Texture[] textures;
-    
+    public Color[] colors;
+
+    Color c1 = new Color(0f, 1f, .80f, 1f);
+    Color c2 = new Color(1f, 0f, .50f, 1f);
+
     // Start is called before the first frame update
     void Start()
     {
@@ -22,5 +26,6 @@ public class TexturesList : MonoBehaviour
     {
         //GetComponent<Renderer>().material.mainTexture = textures[Random.Range(0, textures.Length)];
         GetComponent<Renderer>().material.SetTexture("_Texture2D", textures[Random.Range(0, textures.Length)]);
+        GetComponent<Renderer>().material.SetColor("_Color", colors[Random.Range(0, colors.Length)]);
     }
 }
